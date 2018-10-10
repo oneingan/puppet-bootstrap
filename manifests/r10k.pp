60b2b1d92e8d60fdb3e96e747db9d03833c7430a
José Luis Álvarez-Ossorio <joseluis.alvarezossorio@civir.es>
2018-10-10 10:31:22 +0200

    r10k version control
    Check ruby version to choose r10k version

--- manifests/r10k.pp
+++ manifests/r10k.pp
@@ -1,5 +1,11 @@
+$ruby_version_cmp = versioncmp($facts['ruby']['version'], '2.3.0')
+$r10k_version = $ruby_version_cmp ? {
+  /(1|0)/ => 'latest',
+  '-1' => '3.0.0'
+}
+
 package { 'r10k':
-  ensure   => latest,
+  ensure   => '3.0.0',
   provider => 'puppet_gem',
   source   => $::rubygems_source,
 }
